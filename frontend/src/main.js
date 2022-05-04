29528b739b7643eb693fc853be0ae432f955f850
Jaimy Monsuur <jaimymonsuur@gmail.com>
2022-05-04 12:41:32 +0200

    vue set up

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import Home from './components/Home.vue';
+
+const routes = [
+    { path: '/', component: Home },
+
+];
+
+const router = createRouter({
+    "history": createWebHistory(),
+    routes
+})
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
