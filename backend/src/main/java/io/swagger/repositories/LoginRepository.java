dc6f06e2f9b5c40ea9ebb6607e979e713112c849
Bastiaan <667382@student.inholland.nl>
2022-05-29 10:57:40 +0200

    Added login service and repository

---
+++ backend/src/main/java/io/swagger/repositories/LoginRepository.java
@@ -0,0 +1,12 @@
+package io.swagger.repositories;
+
+import io.swagger.model.entities.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface LoginRepository extends PagingAndSortingRepository<User, UUID> {
+    boolean login(String username, String password);
+}
