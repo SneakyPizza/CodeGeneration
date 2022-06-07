dce7ff475b6516596bfa8432711352d35ff48288
Jaimy Monsuur <jaimymonsuur@gmail.com>
2022-06-07 22:17:18 +0200

    fixed login

--- backend/src/main/java/io/swagger/api/LoginApiController.java
+++ backend/src/main/java/io/swagger/api/LoginApiController.java
@@ -15,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -67,7 +68,12 @@ public class LoginApiController implements LoginApi {
                     }
                 }
             }
-        } catch (Exception e) {
+        }
+        catch (BadCredentialsException e){
+            return new ResponseEntity<ErrorDTO>(new ErrorDTO(LocalDateTime.now().toString(), "Unauthorized: login credentials were invalid", HttpStatus.UNAUTHORIZED.value(), "UNAUTHORIZED"), HttpStatus.UNAUTHORIZED);
+
+        }
+        catch (Exception e) {
             log.error("Internal server error", e);
             return new ResponseEntity<ErrorDTO>(new ErrorDTO(LocalDateTime.now().toString(), "An internal server error had occured!", 500, "INTERNAL_SERVER_ERROR"), HttpStatus.INTERNAL_SERVER_ERROR);
         }
