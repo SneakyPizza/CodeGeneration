230e9c19f6003e73a2a4c22bfa07dd54001224bc
Jaimy Monsuur <jaimymonsuur@gmail.com>
2022-06-21 16:07:02 +0200

    form is nu anders

--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -4,105 +4,113 @@
     <div class="container-lg mt-5">
       <h1 class="blue heading mt-3 mt-lg-3">Inholland bank</h1>
       <div class="row">
-        <div class="card main">
+        <div class="card main_signUp">
           <div class="card-body">
-            <h1 class="card-title">Sign Up User As Admin</h1>
+            <h1 class="card-title">Sign Up</h1>
             <div class="row">
               <div class="col">
                 <div v-if="errorMessage" class="alert alert-danger" role="alert">
                   {{ errorMessage }}
                 </div>
                 <form>
-                  <div class="mb-3">
-                    <label for="inputUsername" class="form-label">Username</label>
-                    <input
-                        id="inputUsername"
-                        type="text"
-                        class="form-control"
-                        v-model="username"
-                    />
+                  <div class="row">
+                    <div class="col-6">
+                      <div class="mb-3">
+                        <label for="inputUsername" class="form-label">Username</label>
+                        <input
+                            id="inputUsername"
+                            type="text"
+                            class="form-control"
+                            v-model="username"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputPassword" class="form-label">Password</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="inputPassword"
+                            v-model="password"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputEmail" class="form-label">Email</label>
+                        <input
+                            type="email"
+                            class="form-control"
+                            id="inputEmail"
+                            v-model="email"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputFirstName" class="form-label">First Name</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="inputFirstName"
+                            v-model="firstName"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputLastName" class="form-label">Last Name</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="inputLastName"
+                            v-model="lastName"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-6">
+
+                      <div class="mb-3">
+                        <label for="inputStreet" class="form-label">Street</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="inputStreet"
+                            v-model="street"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputCity" class="form-label">City</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="inputCity"
+                            v-model="city"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputZipcode" class="form-label">Zipcode</label>
+                        <input
+                            type="text"
+                            class="form-control"
+                            id="inputZipcode"
+                            v-model="zipcode"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputDayLimit" class="form-label">Day Limit</label>
+                        <input
+                            type="number"
+                            class="form-control"
+                            id="inputDayLimit"
+                            v-model="dayLimit"
+                        />
+                      </div>
+                      <div class="mb-3">
+                        <label for="inputTransaction" class="form-label">Transaction Limit</label>
+                        <input
+                            type="number"
+                            class="form-control"
+                            id="inputTransaction"
+                            v-model="transactionLimit"
+                        />
+                      </div>
+                    </div>
                   </div>
-                  <div class="mb-3">
-                    <label for="inputPassword" class="form-label">Password</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="inputPassword"
-                        v-model="password"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="inputEmail" class="form-label">Email</label>
-                    <input
-                        type="email"
-                        class="form-control"
-                        id="inputEmail"
-                        v-model="email"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="inputFirstName" class="form-label">First Name</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="inputFirstName"
-                        v-model="firstName"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="inputLastName" class="form-label">Last Name</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="inputLastName"
-                        v-model="lastName"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="inputStreet" class="form-label">Street</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="inputStreet"
-                        v-model="street"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="inputCity" class="form-label">City</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="inputCity"
-                        v-model="city"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="inputZipcode" class="form-label">Zipcode</label>
-                    <input
-                        type="text"
-                        class="form-control"
-                        id="inputZipcode"
-                        v-model="zipcode"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="inputDayLimit" class="form-label">Day Limit</label>
-                    <input
-                        type="number"
-                        class="form-control"
-                        id="inputDayLimit"
-                        v-model="dayLimit"
-                    />
-                  </div>
-                  <div class="mb-3">
-                    <label for="inputTransaction" class="form-label">Transaction Limit</label>
-                    <input
-                        type="number"
-                        class="form-control"
-                        id="inputTransaction"
-                        v-model="transactionLimit"
-                    />
-                  </div>
+
                   <button type="button" @click="postUser()" class="btn btn-primary">
                     Create user
                   </button>
@@ -152,10 +160,11 @@ export default {
           transactionLimit: this.transactionLimit,
         }
         console.log(user);
-        const response = await LoginService.postUser(user);
-        if (response.data() != null) {
+        const response = await LoginService.signUp(user);
+        if (response.data != null) {
           await this.$router.push('/login')
         }
+        console.log(response.data);
       }
       catch (error) {
         this.errorMessage = error.response.data.message;
@@ -172,9 +181,9 @@ export default {
   font-size: 60px;
   text-align: center;
 }
-.main {
+.main_signUp {
   margin: 0 auto;
-  max-width: 600px;
+  max-width: 1200px;
 }
 
 </style>
